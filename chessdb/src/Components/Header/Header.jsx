24aed324acf6c8943a932fa6ef9ac8d43eee68b6
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-09-02 15:04:53 +0100

    simple header

---
+++ chessdb/src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <button className="header__option">Community Collection</button>
+
+        <button className="header__option">My Collection</button>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
