1c5fa8bc07cc800f3b21dc4cebac0d9e20cb8037
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-09-04 16:53:20 +0100

    User can now add a game to the database.

--- chessdb/src/Components/UploadGameContainer/UploadGameContainer.jsx
+++ chessdb/src/Components/UploadGameContainer/UploadGameContainer.jsx
@@ -1,12 +1,46 @@
 import "./UploadGameContainer.scss";
+import { useState } from "react";
 
 const UploadGameContainer = () => {
+  const [fen, setFen] = useState();
+  const [title, setTitle] = useState();
+
+  const uploadFen = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8080/addGame", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: title, fen: fen }),
+      });
+    } catch (e) {
+      console.log("something went wrong");
+      console.log(e);
+    }
+  };
+
   return (
     <div className="container">
-      <button>Upload game</button>
-
-      <label for="fen">FEN</label>
-      <input id="fen" type="text"></input>
+      <form onSubmit={uploadFen}>
+        <div>
+          <label htmlFor="fen">FEN</label>
+          <input
+            id="fen"
+            type="text"
+            onChange={(e) => setFen(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="fen">Title</label>
+          <input
+            id="fen"
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+        </div>
+        <button type="submit">Upload game</button>
+      </form>
     </div>
   );
 };
