ffde7fe1277b56fa77e0052b13a9f95b63eb31ab
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-09-07 19:47:08 +0100

    Delete button working

---
+++ chessdb/src/Components/DeleteButton/DeleteButton.jsx
@@ -0,0 +1,29 @@
+import "./DeleteButton.scss";
+
+const DeleteButton = ({ gameId }) => {
+  const deleteGame = async (e) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/deleteGame/${gameId}`,
+        {
+          method: "DELETE",
+        }
+      );
+    } catch (e) {
+      console.log("something went wrong");
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className="delete">
+      <form onSubmit={deleteGame}>
+        <button className="delete-button" type="submit">
+          Delete
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default DeleteButton;
